2cd943859b475a5619f86f08923b0c8116757a66
Ataru Ohto <ataruohto@gmail.com>
2023-12-28 22:22:49 +0900

    init vite

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+
+import { Heading, Text } from '@radix-ui/themes'
+
+function App() {
+
+  return (
+    <>
+      <Heading mb="2" size="4">Typographic principles</Heading>
+      <Text>The goal of typography is to relate font size, line height, and line width in a proportional way that maximizes beauty and makes reading easier and more pleasant.</Text>
+    </>
+  )
+}
+
+export default App
